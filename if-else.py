e81443837bc4c17898a82ef9ed168c57de563e4a
Aishashaik16 <153346355+Aishashaik16@users.noreply.github.com>
2025-02-06 12:30:14 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,16 @@
+attack1=int(input("enter atc1 value:"))
+attack2=int(input("enter atc2 value:"))
+health1=int(input("enter energy1:"))
+health2=int(input("enter energy2:"))
+if attack1>attack2:
+    print("warrior1 wins")
+elif attack1<attack2:
+    print("warrior2 wins")
+elif attack1==attack2 or attack1==0 or attack2==0:
+    if health1>health2:
+        print("warrior1 wins based on health")
+    elif health1<health2:
+        print("warrior2 wins based on health")
+    else:
+        print("no attacks and health in both warriors")
+        
